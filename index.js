f995a56eacd928a3c9ae0b8e19fd12d454b1d07d
rubayet-islam <season.ss2001@gmail.com>
2022-11-19 13:28:44 +0600

    remaining slots configuration added with the help of booking query and explanations in the comment

--- index.js
+++ index.js
@@ -24,15 +24,60 @@ async function run() {
       .collection("bookings");
 
     app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
+      console.log(date);
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+      const bookingQuery = { appointmentDate: date }; //created a search query named bookingQuery, with the help of which we are going to search in the bookings database, to find out the bookings created on a particular date (for say in 5 august, 29september, 4 dec)
+      const alreadyBooked = await bookingsCollection
+        .find(bookingQuery)
+        .toArray();
+      //then we executed the query in the bookingCollection to get the bookings (name, time) reserved in that particular date
+
+      //now we forEach the array of objects of the available slots database and take only those options which's name matches which the name we found by filtering out the bookings database
+     
+      options.forEach((option) => {
+        const optionBooked = alreadyBooked.filter(
+          (book) => book.treatment === option.name
+        );
+         //if the names match, then we proceed forward
+        //then we only get the reserved + name matching booked slots for that particular date, for example,
+        // [
+        //   {
+        //     _id: new ObjectId("637877d621168e4ec9b1a3be"),
+        //     appointmentDate: 'Nov 19, 2022',
+        //     treatment: 'Teeth Cleaning',
+        //     patient: 'Season',
+        //     slot: '08.00 AM - 08.30 AM',
+        //   }
+        // ]
+        // [
+        //   {
+        //     _id: new ObjectId("637877df21168e4ec9b1a3bf"),
+        //     appointmentDate: 'Nov 19, 2022',
+        //     treatment: 'Oral Surgery',
+        //     patient: 'Season',
+        //     slot: '09.30 AM - 10.00 AM',
+        //   }
+        // ]
+
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        //now we map each particular date reserved bookings (optionBooked), and take their time slots only
+        
+        const remainingSlots = option.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        //now we match the timeslots, with the appointmentOptions each individual option (its property (slot)) with the bookedSlots we got a step earlier, and to get the remaining slots, we simply use the ! key to get the remaining options
+        option.slots = remainingSlots;
+        //now we change the option.slots as the remaining slots
+      });
       res.send(options);
 
-      app.post('/bookings', async (req,res) => {
+      app.post("/bookings", async (req, res) => {
         const booking = req.body;
         const result = await bookingsCollection.insertOne(booking);
         res.send(result);
-      } )
+      });
     });
   } finally {
   }
