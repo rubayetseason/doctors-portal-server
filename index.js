15207ba0b9de5710998326ebe1f37a1d5224b4f7
rubayet-islam <season.ss2001@gmail.com>
2022-11-19 12:31:38 +0600

    post API endpoint created

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ async function run() {
     const appointmentOptionCollection = client
       .db("doctorsPortal")
       .collection("appointmentOptions");
+    const bookingsCollection = client
+      .db("doctorsPortal")
+      .collection("bookings");
 
     app.get("/appointmentOptions", async (req, res) => {
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
       res.send(options);
+
+      app.post('/bookings', async (req,res) => {
+        const booking = req.body;
+        const result = await bookingsCollection.insertOne(booking);
+        res.send(result);
+      } )
     });
   } finally {
   }
