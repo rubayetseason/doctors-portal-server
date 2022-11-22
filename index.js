55c2530ca8dec39bfb686e0fa6df5d8d337a2012
rubayet-islam <season.ss2001@gmail.com>
2022-11-22 22:01:56 +0600

    id specified bookings data fetching API created

--- index.js
+++ index.js
@@ -115,6 +115,13 @@ async function run() {
       res.send(bookings);
     });
 
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const query = {
@@ -208,6 +215,18 @@ async function run() {
       const result = await doctorsCollection.deleteOne(filter);
       res.send(result);
     });
+
+    // app.get('/addPrice', async (req, res) => {
+    //     const filter = {}
+    //     const options = { upsert: true }
+    //     const updatedDoc = {
+    //         $set: {
+    //             price: 99
+    //         }
+    //     }
+    //     const result = await appointmentOptionCollection.updateMany(filter, updatedDoc, options);
+    //     res.send(result);
+    // })
   } finally {
   }
 }
