515080927e1fbd5fc36ee5ec28667ecbc7b6e025
rubayet-islam <season.ss2001@gmail.com>
2022-11-23 20:54:39 +0600

    stripe intent endpoint created

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const uri =
   "mongodb+srv://docPortal:E37VzqaIAzVhn7Pj@cluster0.tsmlaiu.mongodb.net/test";
@@ -215,6 +216,25 @@ async function run() {
       const result = await doctorsCollection.deleteOne(filter);
       res.send(result);
     });
+
+
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+          currency: 'usd',
+          amount: amount,
+          "payment_method_types": [
+              "card"
+          ]
+      });
+      res.send({
+          clientSecret: paymentIntent.client_secret,
+      });
+  });
 
     // app.get('/addPrice', async (req, res) => {
     //     const filter = {}
